01a9493d8c0cd3ddce327479468d2b4553194137
Augustas <ahaugustas@gmail.com>
2026-05-09 12:50:13 +0300

    added ability to patch or delete a task, query to filter tasks when using GET

--- backend/main.py
+++ backend/main.py
@@ -10,41 +10,76 @@
 # pip install "fastapi[standard]"
 
 #GIT
-# git checkout -b branchName <--- creates new branch
-# git checkout develop <--- switches branches
-# git merge branchName <--- merges branch
+# git checkout develop <--- switches branch to develop
+# git push origin develop <--- pushes changes to develop
+
+# git checkout -b feature-branch <--- creates new branch
+# git merge feature-branch <--- merges branch
 
 # > fastapi dev
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
+# rules for tasks
 class Task(BaseModel):
     id: int | None = None
     title: str
-    completed: bool = False
+    done: bool = False
 
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    done: bool | None = None
+
+# a temporary database
 tasks = [
-    Task(id=0, title="task0", completed=False),
-    Task(id=1, title="task1", completed=False),
-    Task(id=2, title="task2", completed=True)
+    Task(id=0, title="task0", done=False),
+    Task(id=1, title="task1", done=False),
+    Task(id=2, title="task2", done=True)
 ]
 
+# get all tasks (with query parameter)
 @app.get("/tasks")
-def getTasks():
+def getTasks(done: bool | None = None):
+    if done is not None:
+        return [task for task in tasks if task.done == done]
     return tasks
 
-@app.post("/tasks")
-def addTask(task: Task):
-    task.id = len(tasks)
-    tasks.append(task)
-    return task
-
-@app.patch("/tasks/{task_id}")
-def completeTask(task_id: int):
+# get a singular task
+@app.get("/tasks/{task_id}")
+def getTask(task_id: int):
     for task in tasks:
-        if task_id == task.id:
-            task.completed = not task.completed
+        if task.id == task_id:
             return task
+    raise HTTPException(status_code=404, detail="Task not found")
+        
+
+# add a new task
+@app.post("/tasks")
+def addTask(new: Task):
+    new.id = len(tasks)
+    tasks.append(new)
+    return new
+
+# update a task
+@app.patch("/tasks/{task_id}")
+def updateTask(task_id: int, updated: TaskUpdate):
+    for task in tasks:
+        if task.id == task_id:
+            if updated.title is not None:
+                task.title = updated.title
+            if updated.done is not None:
+                task.done = updated.done
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+        
+# delete a task
+@app.delete("/tasks/{task_id}")
+def deleteTask(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            raise HTTPException(status_code=200)
+    raise HTTPException(status_code=404, detail="Task not found")
