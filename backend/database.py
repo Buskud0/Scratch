fa4b3c101b1cf9ac532bd3e41265834f74fe115c
Buskud0 <ahaugustas@gmail.com>
2026-05-13 22:37:00 +0300

    feat(database): .env file added
    Configured the database to use private credentials from the .env file

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-user = "root"
-password = "root"
+load_dotenv()
+db_pass = os.getenv("SCRATCH_DB_PASSWORD")
+db_user = os.getenv("SCRATCH_DB_USER")
+db_name = os.getenv("SCRATCH_DB_NAME")
+
+user = db_user
+password = db_pass
 host = "127.0.0.1"
 port = 3306
-database = "scratchDB"
+database = db_name
 
 URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
